333665424c20533edddd1e1aa2c594c7908f5732
Cody Chan <int64ago@gmail.com>
2018-02-08 11:19:26 +0800

    chore: release v1.0.0

---
+++ index.js
@@ -0,0 +1,23 @@
+const updateNotifier = require('update-notifier');
+const { parse, stringify } = require('ini');
+const { readFileSync, writeFileSync, ensureFileSync } = require('fs-extra');
+const { homedir } = require('os');
+const { join } = require('path');
+
+const NPM_RC = join(homedir(), '.npmrc');
+ensureFileSync(NPM_RC);
+
+module.exports = ({
+  scope = '',
+  registry = 'https://registry.npmjs.org',
+  rewrite = false,
+} = {}) => {
+  if (scope) {
+    const config = parse(readFileSync(NPM_RC, 'utf-8'));
+    if (!config[scope + ':registry'] || rewrite) {
+      config[scope + ':registry'] = registry;
+      writeFileSync(NPM_RC, stringify(config));
+    }
+  }
+  return updateNotifier;
+};
